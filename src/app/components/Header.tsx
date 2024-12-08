58d43115eceb9644e5da8598b4cffbf2d078a0aa
tayyaba <tayyabashahbaz2020@gmail.com>
2024-12-08 15:06:56 +0500

    First commit

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,19 +1,29 @@
-import { MdOutlineMenu } from "react-icons/md";
-import Link from "next/link";
-export default function Header(){
-    return(<div>
-        <ul className="gap-10 lg:gap-16 hidden md:flex  justify-end p-4 ">
-    <li className="menuLink"><Link href="/" >Home</Link></li>
-    <li className="menuLink "><Link  href="/about">About</Link></li>
-    <li className="menuLink"><Link href="/contact">Contact</Link></li>
-    
-     
-</ul>
-
-<MdOutlineMenu className="md:hidden" size={30}/>
-    
-            </div>
-    
-    );
-
-};
+import { IoIosSearch } from "react-icons/io";
+import { RiShoppingCart2Line } from "react-icons/ri";
+import Image from "next/image";
+export default function Nav1(){
+    return(
+        <div >
+            <div className="flex border-b p-3 justify-between ">
+            <div> < IoIosSearch color="purple" size={30}  /> </div>
+             <div className='ml-64 text-xl [color:rgba(42, 37, 75, 1)]'>Avion</div>
+               <div><p className="flex text-purple-800 ml-72 gap-3 "><RiShoppingCart2Line size={30} />
+             <Image src='/header.png' alt="logo" width={20} height={50}/> </p></div>
+            
+        </div>
+        <div>
+            <ul className="flex justify-center gap-6 m-4 text-gray-500">
+                <li>Plant pots</li>
+                <li>Ceramics</li>
+                <li>Tables</li>
+                <li>Chairs</li>
+                <li>Crockery</li>
+                <li>Tableware</li>
+                <li>Cutlery</li>
+                
+            </ul>
+        </div>
+        </div>
+        
+    )
+}
